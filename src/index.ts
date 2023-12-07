9d16109f0a261228820a0f0cc419fa9e3a282e02
kingwang09 <kingwang09@gmail.com>
2023-12-07 11:28:01 +0900

    add subscribe.

--- src/index.ts
+++ src/index.ts
@@ -12,32 +12,71 @@ interface sampleMessage{
 }
 const msgParams = (msg: sampleMessage) => {
     return {
-      MessageBody: JSON.stringify(msg),
-      MessageDeduplicationId: `${msg.key}}`, // Required for FIFO queues
-      MessageGroupId: 'CrawlingGroup', // Required for FIFO queues
-      QueueUrl: CRAWLER_QUEUE_URL,
+        MessageBody: JSON.stringify(msg),
+        MessageDeduplicationId: `${msg.key}}`, // Required for FIFO queues
+        MessageGroupId: 'CrawlingGroup', // Required for FIFO queues
+        QueueUrl: CRAWLER_QUEUE_URL,
     };
-  };
+};
+
+console.log(`hello REGION=${REGION}, ENDPOINT=${ENDPOINT}`);
+
+const config = {
+    endpoint: new AWS.Endpoint(ENDPOINT),//https://sqs.ap-northeast-2.amazonaws.com
+    region: REGION,//ap-northeast-2
+    accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
+    secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
+};
+    
+// Create an SQS service object
+const SQS = new AWS.SQS(config);
 
 const main = () => {
-    console.log(`hello REGION=${REGION}, ENDPOINT=${ENDPOINT}`);
+    //sendMessage({key: 'hello world!!', msg: 'test message'});
 
-    const config = {
-        endpoint: new AWS.Endpoint(ENDPOINT),//https://sqs.ap-northeast-2.amazonaws.com
-        region: REGION,//ap-northeast-2
-        accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
-        secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
-    };
-        
-    // Create an SQS service object
-    const SQS = new AWS.SQS(config);
-    SQS.sendMessage(msgParams({key:'Hello', msg: 'World!!'}), function (err, data) {
-        if (err) {
-          console.log('Error', err);
-        } else {
-          console.log('Success', data.MessageId);
-        }
-      });
+    receiveMessage();
+    
 }   
+
+const sendMessage = (message: sampleMessage) => {
+    SQS.sendMessage(msgParams(message), function (err, data) {
+        if (err) {
+            console.log('Error', err);
+        } else {
+            console.log('Success', data.MessageId);
+        }
+    });
+}
+
+
+const receiveParam = {
+    AttributeNames: ['SentTimestamp'],
+    MaxNumberOfMessages: 1,
+    MessageAttributeNames: ['All'],
+    QueueUrl: CRAWLER_QUEUE_URL,
+    VisibilityTimeout: 20,
+    WaitTimeSeconds: 0,
+};
+
+const receiveMessage = async () => {
+    const data = await SQS.receiveMessage(receiveParam).promise();
+    console.log('receiveMessage: data=', data);
+    if (data.Messages) {
+        //delete receive message
+        const deleteParams = {
+          QueueUrl: CRAWLER_QUEUE_URL,
+          ReceiptHandle: data.Messages[0].ReceiptHandle || '',
+        };
+        SQS.deleteMessage(deleteParams, function (err, data) {
+          if (err) {
+            console.log('Delete Error', err);
+          }
+        });
+      
+        const body = data.Messages[0].Body;
+        console.log('receiveMessage: body=', body);
+    }
+
+}
 
 main();
